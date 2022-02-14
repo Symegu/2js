049b6a67f9c20051dddb506b9666394c921c0ff5
Symegu <89914120+Symegu@users.noreply.github.com>
2022-02-14 15:05:43 +0300

    Дз сделано, но не работает
    Непонятная ошибка

--- 3/project/js/main.js
+++ 3/project/js/main.js
@@ -20,9 +20,10 @@ class ProductsList {
     constructor(container = '.products'){
         this.container = container;
         this.goods = [];//массив товаров из JSON документа
+        this.allProducts = [];
         this._getProducts()
             .then(data => { //data - объект js
-                 this.goods = data;
+                 this.goods = [...data];
 //                 console.log(data);
                  this.render()
             });
@@ -44,7 +45,7 @@ class ProductsList {
        
     }
     calcSum(){
-        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+        return this.allProducts.reduce((acc, item) => acc += item.price, 0);
     }
     render(){
         const block = document.querySelector(this.container);
@@ -78,5 +79,55 @@ class ProductItem {
 }
 
 let list = new ProductsList();
-console.log(list.allProducts);
+
+class Basket {
+    constructor(container = '.cart-block') {
+        this.container = container;
+        this.goods =  [];
+        this._clickBasket();
+        this._getBasketItem()
+            .then(data => {
+                this.goods = data.contents;
+                this.render();
+            });
+    }
+    _getBasketItem () {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    render () {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new BasketItem();
+            block.insertAdjacentHTML('beforeend', productObj.render(product));
+        }
+    }
+    _clickBasket() {
+        document.querySelector(".btn-cart").addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('invisible');
+        })
+    }
+}
+
+class BasketItem {
+    render(product, img = 'https://via.placeholder.com/200x150') {
+        return `<div class="cart-item" data-id="${product.id_product}">
+        <div class="product-bio">
+            <img src="${img}" alt="img">
+            <div class="product-desc">
+                <p class="product-title">${product.product_name}</p>
+                <p class="product-single-price">${product.price}</p>
+                <p class="product-quantity">${product.quantity}</p>
+            </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">${product.price*product.quantity}</p>
+            <button class="del-btn" data-id="${product.id_product}"></button>
+        </div>
+    </div>`
+    }
+}
 
