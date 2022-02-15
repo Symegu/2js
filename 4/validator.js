a9f561a32972968c173217a14aad9be4c630b0ac
Symegu <89914120+Symegu@users.noreply.github.com>
2022-02-15 17:39:33 +0300

    Дз сделано

---
+++ 4/validator.js
@@ -0,0 +1,66 @@
+"use strict";
+class Validator {
+    constructor(form) {
+        this.patterns = {
+            name: /^[a-zа-яё]+$/i,
+            phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
+            email: /^[\w._-]+@\w+\.[a-z]{2,4}$/i
+        };
+        this.errors = {
+            name: 'Имя содержит только буквы',
+            phone: 'Телефон должен соответствовать шаблону +7(123)456-7890',
+            email: 'Email должен соответствовать шаблону mail@mail.mail'
+        };
+        this.errorClass = 'error-msg';
+        this.form = form;
+        this.valid = false;
+        this._validateForm();
+    }
+    validate(regexp, value) {
+        regexp.test(value)
+    }
+    _validateForm() {
+        let errors = [...document.getElementById(this.form).querySelectorAll(`.${this.errorClass}`)];
+        for (let error of errors) {
+            error.remove ();
+        }
+        let formFields = [...document.getElementById(this.form).getElementsByTagName(`input`)];
+        for (let field of formFields) {
+            this._validate(field);
+        }
+        if(![...document.getElementById(this.form).querySelectorAll(`.invalid`)].length) {
+            this.valid = true;
+        }
+    }
+    _validate(field) {
+        if(this.patterns[field.name]) {
+            if(!this.patterns[field.name].test(field.value)) {
+                field.classList.add('invalid');
+                this._addErrorMsg(field);
+                this._watchField(field);
+            }
+        }
+    }
+    _addErrorMsg(field) {
+        let error = `<div class="${this.errorClass}">${this.errors[field.name]}</div>`;
+        field.parentNode.insertAdjacentHTML('beforeend', error);
+    }
+    _watchField(field){
+        field.addEventListener('input', () => {
+            let error = field.parentNode.querySelector(`.${this.errorClass}`);
+            if (this.patterns[field.name].test(field.value)) {
+                field.classList.remove('invalid');
+                field.classList.add('valid');
+                if(error){
+                    error.remove();
+                }
+            } else {
+                field.classList.remove('valid');
+                field.classList.add('invalid');
+                if(!error){
+                    this._addErrorMsg(field);
+                }
+            }
+        })
+    }
+}
